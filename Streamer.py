9dec83f01ffaab14119d6f250c392624e982de83
Nicole Saade <nicolesaadea@gmail.com>
2022-08-04 22:06:41 -0400

    Optimization changes and also included compound score in text files. Implemented translation and analysis code.

--- Streamer.py
+++ Streamer.py
@@ -3,12 +3,14 @@ import Credentials
 import HashtagList
 import nltk
 import nltk.corpus
+from googletrans import Translator
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import sys
 
 
 
 def searcher(hashtags, filename):
+    translator = Translator(['translate.googleapis.com'])
     auth_Handler = tweepy.OAuthHandler(Credentials.Consumer_Key, Credentials.Consumer_Secret)
     auth_Handler.set_access_token(Credentials.Access_Token, Credentials.Access_Token_Secret)
     api = tweepy.API(auth_Handler)
@@ -18,12 +20,14 @@ def searcher(hashtags, filename):
     while i < 4:
         tweets = tweepy.Cursor(api.search_tweets, q=hashtags[i], lang='es').items(3000)
         for tweet in tweets:
-            final_tweet = tweet.text.replace('RT', '')  
+            untrans_final_tweet = tweet.text.replace('RT', '') 
+            final_tweet =  translator.translate(untrans_final_tweet, dest='en')
+            final_tweet = str(final_tweet)
             with open(filename, "r+") as file: 
-                if (final_tweet not in file.read()):
+                if str(final_tweet) not in file.read():
                     tweet_count = tweet_count + 1 
                     comp = analyzer.polarity_scores(final_tweet)['compound']
-                    print(str(comp) + "  || Tweet " + str(tweet_count) + ": " + final_tweet + " \n" , file=file)
+                    print(str(comp) + "  | Tweet " + str(tweet_count) + ": " + final_tweet + " \n" , file=file)
         i = i + 1
         file.close()
 
@@ -31,10 +35,12 @@ def searcher(hashtags, filename):
 def main():
     
 
-    ##searcher(HashtagList.hashtags_Ecuador, 'EcuadorTweets.txt')
+    searcher(HashtagList.hashtags_Ecuador, 'EcuadorTweets.txt')
     searcher(HashtagList.hashtags_Peru, 'PeruTweets.txt')
-    ##searcher(HashtagList.hashtags_Argentina, 'ArgentinaTweets.txt')
-    ##searcher(HashtagList.hashtags_Chile, 'ChileTweets.txt')
+    searcher(HashtagList.hashtags_Argentina, 'ArgentinaTweets.txt')
+    searcher(HashtagList.hashtags_Chile, 'ChileTweets.txt')
+    searcher(HashtagList.hashtags_Colombia, 'ColombiaTweets.txt')
+
 
 
 if __name__ == '__main__':
