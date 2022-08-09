9f7bb006ae7bf76e8d64a8f1c0d976f1c781fad1
Nicole Saade <62183652+nicolesaade@users.noreply.github.com>
2022-08-09 12:30:44 -0400

    Delete Credentials.py

--- Credentials.py
+++
@@ -1,7 +0,0 @@
-#Variables containing credentials to access the API
-
-Access_Token = "817388508658749440-pGhrZ5W9hP0Mb2Oz1xL5n08qs93aN3S"
-Access_Token_Secret = "PvohxcEsHv6zDmgT7qT49DRqwiONEtGxXUntq0DuZCeBQ"
-Consumer_Key = "XfTqGcL3CTlUAmqS99FbyG77P"
-Consumer_Secret = "OLf4IKxF1g6VLdI1DO0uWWylfNaZ5yGwvSciLAvkomMVM9EdsF"
-
